e8fb873a047e1f1e891f5a2ea5bbacbbd01d1acf
nasergamal <naser.gamal98@gmail.com>
2023-11-28 22:40:18 +0200

    2- typo fix 6- return array modified

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,17 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results);
+    .then((results) => {
+      const result = [];
+      for (const i of results) {
+        const dict = { status: i.status };
+        if (i.status === 'rejected') {
+          dict.value = i.reason.message;
+        } else {
+          dict.value = i.value;
+          result.push(dict);
+        }
+      }
+      return result;
+    });
 }
